21cdd8df20fb89edff8ec1372d52722cd12d3cb5
anandazp <anandazulvansyahputra@gmail.com>
2026-06-09 09:07:05 +0700

    add code and preview for button components,

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,4 +1,10 @@
 export const siteData = {
   brand: "Tempa UI",
-  tagline: "Simple framework to enhance your user interface",
+  tagline: "Simple UI for lazy developers",
+  description:
+    "Free UI components built with Astro, Typescript and Tailwind CSS.",
+  navigation: [
+    { label: "Docs", href: "/docs" },
+    { label: "Components", href: "/components" },
+  ],
 };
